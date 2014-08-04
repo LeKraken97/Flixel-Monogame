544cb2a60e97cd1b488ae5b0744dfc16f7a7e4d6
Mury <muresanvladmihail@gmail.com>
2014-08-04 17:10:06 +0300

    This is my first commit

---
+++ TestFlixel/TestFlixel/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace org.flixel
+{
+    partial class FlxGame : DrawableGameComponent
+    {
+        public FlxGame()
+            : base(FlxG.Game)
+        {
+            initGame(320, 240, new Mode.State(), new Color(0x13, 0x1c, 0x1b), false, new Color(0x3a, 0x5c, 0x39));
+            help("Jump", "Shoot", "Nothing", "Move");
+        }
+    }
+}
